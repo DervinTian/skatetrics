0cfb088d0ace2dcfed4d45e1592492e21801110c
Dervin Tian <dervint@umich.edu>
2026-01-12 12:15:28 -0500

    Initialized Next App

---
+++ Skate park (with skate board) - 7049008/README.txt
@@ -0,0 +1,4 @@
+Skate park (with skate board) by kyran123456789 on Thingiverse: https://www.thingiverse.com/thing:7049008
+
+Summary:
+:)
---
+++ skatetrics-app/src/components/three/mainCanvas.jsx
@@ -0,0 +1,24 @@
+'use client'
+import {Environment, OrbitControls} from "@react-three/drei"
+import {Canvas} from "@react-three/fiber"
+
+export function MainCanvas() {
+    return (
+        <div className="w-screen h-screen fixed top-0 left-0">
+            <Canvas
+                shadow
+                dpr={[1, 2]}
+                camera={{fov: 55, position: [0, 0, 6]}}
+            >
+                <Environment files="/images/hill.jpg" />
+                <mesh>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color="red"/>
+                </mesh>
+
+                <OrbitControls />
+
+            </Canvas>
+        </div>
+    );
+}
